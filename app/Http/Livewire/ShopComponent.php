cd434bf622561c12eb948bf280e54775bab7ed93
fatemeNemati <nemati.fateme77@gmail.com>
2021-03-29 19:45:06 +0430

    sorting products and choose pagesize

--- app/Http/Livewire/ShopComponent.php
+++ app/Http/Livewire/ShopComponent.php
@@ -10,6 +10,13 @@ use Cart;
 class ShopComponent extends Component
 {
     use WithPagination;
+    public $sorting;
+    public $pageSize;
+
+    public function mount(){
+        $this->sorting ='default';
+        $this->pageSize ='12';
+    }
 
     public function store($produc_id,$product_name,$product_price){
         Cart::add($produc_id,$product_name,1,$product_price)->associate('App\Models\product');
@@ -19,7 +26,18 @@ class ShopComponent extends Component
 
     public function render()
     {
-        $products = Product::paginate(12);
+        if($this->sorting == "date"){
+            $products = Product::orderBy('created_at','DESC')->paginate($this->pageSize);
+        }
+        elseif($this->sorting == "price"){
+            $products = Product::orderBy('regular_price','ASC')->paginate($this->pageSize);
+        }
+        elseif($this->sorting == "price-desc"){
+            $products = Product::orderBy('regular_price','DESC')->paginate($this->pageSize);
+        }
+        else{
+            $products = Product::paginate($this->pageSize);
+        }
         return view('livewire.shop-component',['products'=>$products])->layout('layouts.base');
     }
 }
