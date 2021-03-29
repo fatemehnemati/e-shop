53b7fbc36ac0519f9a75080042cb1e7ca8abae1a
fatemeNemati <nemati.fateme77@gmail.com>
2021-03-29 12:27:21 +0430

    add shop,car and checkout template

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Livewire\HomeComponent;
-
+use App\Http\Livewire\ShopComponent;
+use App\Http\Livewire\CartComponent;
+use App\Http\Livewire\CheckoutComponent;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -19,3 +21,9 @@ use App\Http\Livewire\HomeComponent;
 // });
 
 Route::get('/',HomeComponent::class);
+
+Route::get('/shop',ShopComponent::class);
+
+Route::get('/cart',CartComponent::class);
+
+Route::get('/checkout',CheckoutComponent::class);
