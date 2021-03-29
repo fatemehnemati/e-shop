04aa5616803227a416f458d104108c6fa0f16f32
fatemeNemati <nemati.fateme77@gmail.com>
2021-03-29 19:11:54 +0430

    update cart quantity

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -176,7 +176,7 @@
 									<i class="fa fa-shopping-basket" aria-hidden="true"></i>
 									<div class="left-info">
 										@if(Cart::count() > 0)
-										<span class="index">{{ Cart::count() }}</span>
+										<span class="index">{{ Cart::count() }} Item</span>
 										@endif
 										<span class="title">CART</span>
 									</div>
