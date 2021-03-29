7e7d566c726b49111c3a76bc0f8ba197dd21e1d6
fatemeNemati <nemati.fateme77@gmail.com>
2021-03-29 19:21:39 +0430

    make delete cart item (one by one , clear the list)

--- app/Http/Livewire/CartComponent.php
+++ app/Http/Livewire/CartComponent.php
@@ -17,6 +17,15 @@ class CartComponent extends Component
         $qty = $product->qty-1;
         Cart::update($rowId,$qty);
     }
+    public function destroy($rowId){
+        Cart::remove($rowId);
+        session()->flash('success_message','Item has been removed');
+    }
+    public function destroyAll(){
+        Cart::destroy();
+        session()->flash('success_message','All Items has been removed');
+
+    }
     public function render()
     {
         return view('livewire.cart-component')->layout('layouts.base');
